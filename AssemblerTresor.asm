66611e7b560bd07b0736307427044a8f774022e2
Norman <rickey00x@gmail.com>
2022-05-05 11:31:59 +0200

    add: open_tresor function

--- AssemblerTresor.asm
+++ AssemblerTresor.asm
@@ -1,15 +1,19 @@
+;initial setup to test open function
+init_test_open:
 mov a, #11111111b
 clr a.5
 mov r6, a
 mov P3, r6
 
 open_tresor:
-clr a.0
-setb a.5
+
+mov a, r6	;write r6 into akku
+clr a.0		;set open bit to true
+setb a.5	;clear failed tries
 setb a.6
 setb a.7
-mov r6,a
-mov P3, r6
+mov r6,a	;write akku back into r6
+mov P3, r6	;output r6 on Port 3
 
 end
 
