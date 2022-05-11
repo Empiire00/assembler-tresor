f9e9fa05a73d3d18148a792f0c8cc6517543ca0e
Ricky <75086561+rickey00x@users.noreply.github.com>
2022-05-11 10:49:44 +0200

    added: inverted output

--- AssemblerTresor.asm
+++ AssemblerTresor.asm
@@ -1,19 +1,24 @@
 ;initial setup to test open function
 init_test_open:
-mov a, #11111111b
-clr a.5
+mov a, #00000000b
+setb a.5
 mov r6, a
 mov P3, r6
 
 open_tresor:
 
 mov a, r6	;write r6 into akku
-clr a.0		;set open bit to true
-setb a.5	;clear failed tries
-setb a.6
-setb a.7
+setb a.0		;set open bit to true
+;clear failed tries
+clr a.5	
+clr a.6
+clr a.7
+
 mov r6,a	;write akku back into r6
-mov P3, r6	;output r6 on Port 3
+xrl a, #11111111b ;invert byte for output
+mov P3, a	;output a on Port 3
+
+
 
 end
 
